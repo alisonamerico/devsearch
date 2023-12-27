31b7b7d74c5cb3e3cada845a07da40f67b1c04c6
alisonamerico <alisonlira@proton.me>
2023-12-27 20:28:54 -0300

    Create template projects app

--- projects/urls.py
+++ projects/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from projects.views import project, projects
 
 urlpatterns = [
-    path('projects/', projects, name='projects'),
+    path('', projects, name='projects'),
     path('project/<str:pk>/', project, name='project'),
 ]
