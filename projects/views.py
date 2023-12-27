542b9581523735d0edaf25cd9de151fc6647266e
alisonamerico <alisonlira@proton.me>
2023-12-27 20:28:54 -0300

    Create template projects app

--- projects/views.py
+++ projects/views.py
@@ -1,9 +1,34 @@
-from django.shortcuts import HttpResponse
+from django.shortcuts import render
+
+projects_list = [
+    {
+        'id': '1',
+        'title': 'Ecommerce Website',
+        'description': 'Fully functional ecommerce website',
+    },
+    {
+        'id': '2',
+        'title': 'Portfolio Website',
+        'description': 'A personal website to write articles and display work',
+    },
+    {
+        'id': '3',
+        'title': 'Social Network',
+        'description': 'An open source project built by the community',
+    },
+]
 
 
 def projects(request):
-    return HttpResponse('Here are our projects')
+    page = 'projects'
+    number = 11
+    context = {'page': page, 'number': number, 'projects': projects_list}
+    return render(request, 'projects/projects.html', context)
 
 
 def project(request, pk):
-    return HttpResponse(f'SINGLE PROJECT {pk}')
+    project_obj = None
+    for project in projects_list:
+        if project['id'] == pk:
+            project_obj = project
+    return render(request, 'projects/single-project.html', {'project': project_obj})
