94d1e8dac82bcb5d10c7da495884859876856ab0
alisonamerico <alisonlira@proton.me>
2024-01-01 12:46:10 -0300

    Create a model form for Project

--- projects/urls.py
+++ projects/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from projects.views import project, projects
+from projects.views import project, projects, create_project
 
 urlpatterns = [
     path('', projects, name='projects'),
     path('project/<str:pk>/', project, name='project'),
+    path('create-project/', create_project, name='create-project'),
 ]
