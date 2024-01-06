8021f1d9c528ea9de963918d3bb6a916ad36edbf
alisonamerico <alisonlira@proton.me>
2024-01-06 18:15:55 -0300

    Creates crud for project

--- projects/urls.py
+++ projects/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from projects.views import project, projects, create_project
+from projects.views import create_project, delete_project, project, projects, update_project
 
 urlpatterns = [
     path('', projects, name='projects'),
     path('project/<str:pk>/', project, name='project'),
     path('create-project/', create_project, name='create-project'),
+    path('update-project/<str:pk>/', update_project, name='update-project'),
+    path('delete-project/<str:pk>/', delete_project, name='delete-project'),
 ]
