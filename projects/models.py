db17ead10eac41da9ac09c532ee7a962292f8845
alisonamerico <alisonlira@proton.me>
2024-01-18 23:29:38 -0300

    Create users app Closes #19

--- projects/models.py
+++ projects/models.py
@@ -2,11 +2,14 @@ import uuid
 
 from django.db import models
 
+from users.models import Profile
+
 
 class Project(models.Model):
     id = models.UUIDField(
         default=uuid.uuid4, unique=True, primary_key=True, editable=False
     )
+    owner = models.ForeignKey(Profile, null=True, blank=True, on_delete=models.SET_NULL)
     title = models.CharField(max_length=200)
     description = models.TextField(null=True, blank=True)
     featured_image = models.ImageField(
