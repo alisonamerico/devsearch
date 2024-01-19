f3f29bf272662a9371ddd666b31f22d3b3f5e058
alisonamerico <alisonlira@proton.me>
2024-01-18 23:29:38 -0300

    Create users app Closes #19

--- devsearch/settings.py
+++ devsearch/settings.py
@@ -40,11 +40,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'projects',
+    'users',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    "whitenoise.middleware.WhiteNoiseMiddleware",
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
---
+++ users/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from users.views import profiles
+
+urlpatterns = [
+    path('', profiles, name='profiles'),
+]
