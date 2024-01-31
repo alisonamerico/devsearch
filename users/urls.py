06430e8d4c5928b469e58ad8e68540e0a4765574
alisonamerico <alisonlira@proton.me>
2024-01-31 10:16:33 -0300

    Create login, logout and flash message

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from users.views import profiles, user_profile
+from users.views import login_user, profiles, user_profile, logout_user
 
 urlpatterns = [
+    path('login/', login_user, name='login'),
+    path('logout/', logout_user, name='logout'),
     path('', profiles, name='profiles'),
     path('profile/<str:pk>/', user_profile, name='user-profile'),
 ]
