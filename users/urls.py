9405bd82e1b8ede17977fc9825e33fd7adfa6c23
alisonamerico <alisonlira@proton.me>
2024-02-05 06:18:12 -0300

    Create user registration

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,16 @@
 from django.urls import path
-from users.views import login_user, profiles, user_profile, logout_user
+from users.views import (
+    login_user,
+    profiles,
+    register_user,
+    user_profile,
+    logout_user,
+)
 
 urlpatterns = [
     path('login/', login_user, name='login'),
     path('logout/', logout_user, name='logout'),
+    path('register/', register_user, name='register'),
     path('', profiles, name='profiles'),
     path('profile/<str:pk>/', user_profile, name='user-profile'),
 ]
