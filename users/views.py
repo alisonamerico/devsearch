4c18d0112ea5fde47ec95d43af9b3f2f90cebe4b
alisonamerico <alisonlira@proton.me>
2024-02-05 06:18:12 -0300

    Create user registration

--- users/views.py
+++ users/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render, redirect
+from users.forms import CustomUserCreationForm
 from users.models import Profile
 from django.contrib.auth.models import User
 from django.contrib.auth import login, authenticate, logout
 from django.contrib import messages
 
+
 def login_user(request):
+    page = 'login'
 
     if request.user.is_authenticated:
         return redirect('profiles')
@@ -29,6 +32,29 @@ def logout_user(request):
     logout(request)
     messages.error(request, 'User was logged out!')
     return redirect('login')
+
+
+def register_user(request):
+    page = 'register'
+    form = CustomUserCreationForm()
+
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+
+            messages.success(request, 'User account was created!')
+
+            login(request, user)
+            return redirect('profiles')
+
+    else:
+        messages.error(request, 'An error has occurred during registration')
+
+    context = {'page': page, 'form': form}
+    return render(request, 'users/login_register.html', context)
 
 
 def profiles(request):
